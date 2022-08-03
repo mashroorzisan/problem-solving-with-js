8c8cd17f9dfc464722b0e0d53a69ef885c9c8a9f
mashroorzisan <mashroorzisan@gmail.com>
2022-08-03 23:52:37 +0600

    prblm 2

---
+++ prb2.js
@@ -0,0 +1,16 @@
+// variable declaration
+var mathmatics;
+var bangla;
+var biology;
+var chemistry;
+var physics;
+// ***value declaration***
+mathmatics = 82;
+chemistry = 90;
+bangla = 75;
+physics = 84;
+biology = 89;
+// ***average***
+var agerage;
+average = (mathmatics + bangla + biology + chemistry + physics) / 5;
+console.log(average);
